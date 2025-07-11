b7fa7b21c08bb56ed4d5a39c67bd3f39d70106b7
Magdalena Kołtuniak <m.koltuniak@gmail.com>
2025-07-11 14:19:59 +0200

    added binary calculator

--- calculator v1/calculator_4.js
+++ calculator v1/calculator_4.js
@@ -67,5 +67,69 @@ class DecimalCalculator extends Calculator {
 	}
 }
 
+class BinaryCalculator extends Calculator {
+	#operationHistory = [];
+
+	calculate(a, b, sign) {
+		const aDec = this.bin2dec(a);
+		const bDec = this.bin2dec(b);
+
+		if (isNaN(a) || isNaN(b)) return 'Niepoprawne wartości. Podaj liczbę.';
+
+		switch (sign) {
+			case '+':
+				return `${a} + ${b} = ${this.dec2bin(aDec + bDec)}`;
+			case '-':
+				return `${a} - ${b} = ${this.dec2bin(aDec - bDec)}`;
+			case '*':
+				return `${a} * ${b} = ${this.dec2bin(aDec * bDec)}`;
+			case '/': {
+				if (bDec === 0) return 'Nie można dzielić przez 0';
+				return `${a} / ${b} = ${this.dec2bin(aDec / bDec)}`;
+			}
+			default:
+				return 'Nieobsługiwana operacja.';
+		}
+	}
+
+	bin2dec(val) {
+		return parseInt(val, 2);
+	}
+
+	dec2bin(val) {
+		return val.toString(2);
+	}
+
+	addToHistory(operation) {
+		this.#operationHistory.push(operation);
+	}
+
+	printHistory() {
+		this.#operationHistory.forEach((operation, index) => {
+			console.log(`Operacja nr ${index + 1}: ${operation}`);
+		});
+	}
+
+	run() {
+		let choice;
+		do {
+			const val1 = prompt('Podaj pierwszą liczbę w systemie dwójkowym');
+			const val2 = prompt('Podaj drugą liczbę w systemie dwójkowym');
+			const operation = prompt('Podaj znak (+, -, *, /).');
+
+			const result = this.calculate(val1, val2, operation);
+			console.log(result);
+			this.addToHistory(operation);
+
+			choice = prompt('Jak chcesz zakończyć wpisz N');
+		} while (choice !== 'N');
+
+		this.printHistory();
+	}
+}
+
 const decimalCalculator = new DecimalCalculator();
-decimalCalculator.run();
+// decimalCalculator.run();
+
+const binaryCalculator = new BinaryCalculator();
+binaryCalculator.run();
