67f60b17a85dc78d24185f78c16595993400a7d0
Magdalena Kołtuniak <m.koltuniak@gmail.com>
2025-07-29 16:55:56 +0200

    added object oriented calculator

---
+++ calculator v1/calculator_5.js
@@ -0,0 +1,49 @@
+class Operation {
+	#callback;
+	constructor(callback) {
+		this.#callback = callback;
+	}
+
+	perform(a, b) {
+		return this.#callback(a, b);
+	}
+}
+
+class Calculator {
+	#operations;
+
+	constructor() {
+		this.#operations = {
+			'+': new Operation((a, b) => a + b),
+			'-': new Operation((a, b) => a - b),
+			'*': new Operation((a, b) => a * b),
+			'/': new Operation((a, b) => {
+				if (b === 0) return 'Nie dziel przez 0!';
+				return a / b;
+			}),
+		};
+	}
+
+	getSign(operation) {
+		if (operation.includes('+')) return '+';
+		if (operation.includes('-')) return '-';
+		if (operation.includes('*')) return '*';
+		if (operation.includes('/')) return '/';
+	}
+
+	getValues(operation, sign) {
+		const values = operation.split(sign).map(val => parseInt(val));
+		return values;
+	}
+
+	calculate(operation) {
+		const sign = this.getSign(operation);
+		const values = this.getValues(operation, sign);
+
+		const result = this.#operations[sign].perform(...values);
+
+		return result;
+	}
+}
+
+const calculator = new Calculator();
